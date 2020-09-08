e851cd9f3b5e10ea8b14dcc44fa5c25eb435bc5a
Alexander <alexander.v.dorohov@yandex.ru>
2020-09-08 12:11:02 +0300

    Добавил тестирование доступных пользователю цветов категорий продуктов.

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './sagas/root';
-import {testReducer} from './reducers/test/test';
+import {testReducer} from './reducers/test/testReducer';
 import {shoppingListsReducer} from './reducers/shopping-lists/shoppingListsReducer';
 import {productsListReducer} from './reducers/products-list/productsListReducer';
 import {categoriesReducer} from './reducers/categories/categoriesReducer';
