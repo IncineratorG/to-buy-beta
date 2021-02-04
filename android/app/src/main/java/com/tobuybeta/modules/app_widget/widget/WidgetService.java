d39da9a561e1d36831a88e75b97a3a20c5e9d05f
Alexander <alexander.v.dorohov@yandex.ru>
2021-02-04 19:30:12 +0300

    Работа над виджетом для приложения.

--- android/app/src/main/java/com/tobuybeta/modules/app_widget/widget/WidgetService.java
+++ android/app/src/main/java/com/tobuybeta/modules/app_widget/widget/WidgetService.java
@@ -3,10 +3,6 @@ package com.tobuybeta.modules.app_widget.widget;
 import android.content.Intent;
 import android.widget.RemoteViewsService;
 
-/**
- * TODO: Add a class header comment
- */
-
 public class WidgetService extends RemoteViewsService {
     @Override
     public RemoteViewsFactory onGetViewFactory(Intent intent) {
